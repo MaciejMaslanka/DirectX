5c96228c73ee623a7955d1ba2375c47456e448b5
MaciejMaslanka <maciej_maslanka@outlook.com>
2016-10-10 21:36:23 +0200

    DirectX

---
+++ RenderowanieKlatek.cpp
@@ -0,0 +1,187 @@
+
+#include <windows.h>
+#include <windowsx.h>
+#include <d3d11.h>
+#include <d3dx11.h>
+#include <d3dx10.h>
+
+
+#pragma comment (lib, "d3d11.lib")
+#pragma comment (lib, "d3dx11.lib")
+#pragma comment (lib, "d3dx10.lib")
+
+
+IDXGISwapChain *swapchain;            
+ID3D11Device *dev;                    
+ID3D11DeviceContext *devcon;          
+ID3D11RenderTargetView *backbuffer;   
+
+
+void InitD3D(HWND hWnd);    
+void RenderFrame(void);     
+void CleanD3D(void);        
+
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance,
+                   HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine,
+                   int nCmdShow)
+{
+    HWND hWnd;
+    WNDCLASSEX wc;
+
+    ZeroMemory(&wc, sizeof(WNDCLASSEX));
+
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+    wc.lpszClassName = L"WindowClass";
+
+    RegisterClassEx(&wc);
+
+    RECT wr = {0, 0, 800, 600};
+    AdjustWindowRect(&wr, WS_OVERLAPPEDWINDOW, FALSE);
+
+    hWnd = CreateWindowEx(NULL,
+                          L"WindowClass",
+                          L"Our First Direct3D Program",
+                          WS_OVERLAPPEDWINDOW,
+                          300,
+                          300,
+                          wr.right - wr.left,
+                          wr.bottom - wr.top,
+                          NULL,
+                          NULL,
+                          hInstance,
+                          NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+
+
+    InitD3D(hWnd);
+
+
+
+    MSG msg;
+
+    while(TRUE)
+    {
+        if(PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+
+            if(msg.message == WM_QUIT)
+                break;
+        }
+
+        RenderFrame();
+    }
+
+    
+    CleanD3D();
+
+    return msg.wParam;
+}
+
+
+// this is the main message handler for the program
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch(message)
+    {
+        case WM_DESTROY:
+            {
+                PostQuitMessage(0);
+                return 0;
+            } break;
+    }
+
+    return DefWindowProc (hWnd, message, wParam, lParam);
+}
+
+
+
+void InitD3D(HWND hWnd)
+{
+    DXGI_SWAP_CHAIN_DESC scd;
+
+  
+    ZeroMemory(&scd, sizeof(DXGI_SWAP_CHAIN_DESC));
+
+
+    scd.BufferCount = 1;                                   
+    scd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;    
+    scd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;    
+    scd.OutputWindow = hWnd;                               
+    scd.SampleDesc.Count = 1;                              
+    scd.SampleDesc.Quality = 0;                            
+    scd.Windowed = TRUE;                                   
+
+
+    D3D11CreateDeviceAndSwapChain(NULL,
+                                  D3D_DRIVER_TYPE_HARDWARE,
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  NULL,
+                                  D3D11_SDK_VERSION,
+                                  &scd,
+                                  &swapchain,
+                                  &dev,
+                                  NULL,
+                                  &devcon);
+
+
+    ID3D11Texture2D *pBackBuffer;
+    swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&pBackBuffer);
+
+   
+    dev->CreateRenderTargetView(pBackBuffer, NULL, &backbuffer);
+    pBackBuffer->Release();
+
+
+    devcon->OMSetRenderTargets(1, &backbuffer, NULL);
+
+
+
+    D3D11_VIEWPORT viewport;
+    ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+
+    viewport.TopLeftX = 0;
+    viewport.TopLeftY = 0;
+    viewport.Width = 800;
+    viewport.Height = 600;
+
+    devcon->RSSetViewports(1, &viewport);
+}
+
+
+
+void RenderFrame(void)
+{
+
+    devcon->ClearRenderTargetView(backbuffer, D3DXCOLOR(0.0f, 0.2f, 0.4f, 1.0f));
+
+
+
+    
+    swapchain->Present(0, 0);
+}
+
+
+
+void CleanD3D(void)
+{
+    swapchain->Release();
+    backbuffer->Release();
+    dev->Release();
+    devcon->Release();
+}
